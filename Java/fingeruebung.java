1444a4fb24b40963deba034d88cfa24699ce8d13
nanid <nanid@tuta.io>
2024-12-02 20:17:52 +0100

    Fingerübung lineare Suche im Array

--- Java/fingeruebung.java
+++ Java/fingeruebung.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
@@ -23,10 +25,11 @@ public class fingeruebung extends Application {
     // end components
 
     primaryStage.setOnCloseRequest(e -> System.exit(0));
-    primaryStage.setTitle("uebnung-vorlage");
+    primaryStage.setTitle("sort");
     primaryStage.setScene(scene);
     primaryStage.show();
-  } // end of public uebnung
+    dostuff();
+  } // end of public sort
 
   // start methods
 
@@ -34,5 +37,55 @@ public class fingeruebung extends Application {
     launch(args);
   } // end of main
 
+  void dostuff() {
+    int[] arr = new int[20];
+    for (int i = 0; i < arr.length; i++) {
+      arr[i] = new Random().nextInt(100);
+    }
+    printarr(arr);
+    find(arr, arr[new Random().nextInt(arr.length)]);
+    // insertion(arr);
+    System.exit(0);
+  }
+
+  int find(int[] arr, int num) {
+    System.out.println("suche: " + num);
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] == num) {
+        System.out.println("Index: " + i);
+        return i;
+      }
+    }
+    return -1;
+  }
+
+  void insertion(int[] arr) {
+    for (int i = 0; i < arr.length; i++) {
+      int val = arr[i];
+      int index = -1;
+      for (int j = 0; j < i; j++) {
+        if (val < arr[j]) {
+          index = j;
+          break;
+        }
+      }
+
+      if (index != -1 && index != i) {
+        for (int k = i; k > index; k--) {
+          arr[k] = arr[k - 1];
+        }
+        arr[index] = val;
+      }
+    }
+    System.out.println("insertion");
+    printarr(arr);
+  }
+
+  void printarr(int[] arr) {
+    for (int i = 0; i < arr.length; i++) {
+      System.out.println(arr[i]);
+    }
+  }
+
   // end methods
-} // end of class fingeruebung
+} // end of class sort
