c808a339314b94e24c885973a6dac0b5671d15f8
nanid <nanid@tuta.io>
2024-12-09 17:51:29 +0100

    implemented binary search on sorted array

--- Java/fingeruebung.java
+++ Java/fingeruebung.java
@@ -1,10 +1,5 @@
 import java.util.Random;
 
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
-import javafx.stage.Stage;
-
 /**
  *
  * Description
@@ -13,39 +8,61 @@ import javafx.stage.Stage;
  * @author
  */
 
-public class fingeruebung extends Application {
+public class fingeruebung {
   // start attributes
   // end attributes
-
-  public void start(Stage primaryStage) {
-    Pane root = new Pane();
-    Scene scene = new Scene(root, 284, 262);
-    // start components
-
-    // end components
-
-    primaryStage.setOnCloseRequest(e -> System.exit(0));
-    primaryStage.setTitle("sort");
-    primaryStage.setScene(scene);
-    primaryStage.show();
-    dostuff();
-  } // end of public sort
-
+  int stepsCount = 0;
   // start methods
 
   public static void main(String[] args) {
-    launch(args);
+    fingeruebung programm = new fingeruebung();
+    programm.dostuff();
   } // end of main
 
-  void dostuff() {
+  public void dostuff() {
     int[] arr = new int[20];
-    for (int i = 0; i < arr.length; i++) {
-      arr[i] = new Random().nextInt(100);
+    int randRange = 50;
+    // fill array sorted with rand. numbers
+    arr[0] = new Random().nextInt(randRange);
+    for (int i = 1; i < arr.length; i++) {
+      arr[i] = arr[i - 1] + new Random().nextInt(randRange);
     }
+
+    int halfIndex = arr.length / 2;
+    int toSearch = arr[new Random().nextInt(arr.length)];
     printarr(arr);
-    find(arr, arr[new Random().nextInt(arr.length)]);
+
+    stepsCount = 0;
+    System.out.println("--- to search: " + toSearch);
+    int ind = binarySearch(arr, halfIndex, (double) halfIndex, toSearch);
+    System.out.println("--- found at index: " + ind + ": " + arr[ind]);
+    System.out.println("--- step needed: " + stepsCount);
+    // find(arr, arr[new Random().nextInt(arr.length)]);
     // insertion(arr);
     System.exit(0);
+  }
+
+  int binarySearch(int arr[], int halfIndex, double step, int num) {
+    System.out.println(halfIndex);
+    if (halfIndex >= 0 && halfIndex <= arr.length && step > 0) {
+      stepsCount++;
+      if (arr[halfIndex] == num) {
+        return halfIndex;
+      }
+      if (step > 1) {
+        // step = (step + 1) / 2; // step +1 to improve rounding and reduce steps taken
+        step = step / 2; // more steps becaus faster down rounding
+      }
+      System.out.println("-- step " + step);
+      if (num > arr[halfIndex]) {
+        return binarySearch(arr, halfIndex + (int) Math.round(step), step, num);
+      } else {
+        return binarySearch(arr, halfIndex - (int) Math.round(step), step, num);
+      }
+
+    }
+
+    return -1;
   }
 
   int find(int[] arr, int num) {
@@ -83,7 +100,7 @@ public class fingeruebung extends Application {
 
   void printarr(int[] arr) {
     for (int i = 0; i < arr.length; i++) {
-      System.out.println(arr[i]);
+      System.out.println(i + ": " + arr[i]);
     }
   }
 
