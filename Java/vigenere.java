ec8f969453e4e3d7ddd3827d1c807f62ac13ca2f
nanid <nanid@tuta.io>
2025-10-01 22:11:20 +0200

    added php stuff

--- Java/vigenere.java
+++ Java/vigenere.java
@@ -1,31 +1,32 @@
 import java.util.Scanner;
 
-public class vigenere{
+public class vigenere {
   // start attributes
   static String alphabet = "abcdefghijklmnopqrstuvwxyz";
   static Scanner scanner;
   static String text = "";
   static boolean encrypt = true; // otherwise decrypt
-  //static String[] alphabet = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
+  // static String[] alphabet =
+  // {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
 
   // end attributes
   // start methods
 
   public static void main(String[] args) {
-    if(args.length > 0){
+    if (args.length > 0) {
       encrypt = false; // decrypt instead
     }
     scanner = new Scanner(System.in);
     boolean encryptAgain = cli();
-    while(encryptAgain){
+    while (encryptAgain) {
       System.out.print("\033\143");
       encryptAgain = cli();
     }
     scanner.close();
   } // end of main
 
-  static boolean cli(){
-    if(encrypt)
+  static boolean cli() {
+    if (encrypt)
       System.out.println("*** ENCRYPTING ***");
     else
       System.out.println("*** DECRYPTING ***");
@@ -35,11 +36,14 @@ public class vigenere{
     scanner = new Scanner(System.in);
     System.out.println("enter key: ");
     key = scanner.nextLine();
+    key = key.toLowerCase();
 
-    if(text.isBlank()){
+    if (text.isBlank()) {
+      System.out.println("---");
       System.out.println("enter text: ");
       text = scanner.nextLine();
-    }else{
+      text = text.toLowerCase();
+    } else {
       System.out.println("text entered:");
       System.out.println(text);
     }
@@ -49,24 +53,24 @@ public class vigenere{
     String resultText = "";
     for (int i = 0; i < text.length(); i++) {
       char currentChar = text.charAt(i);
-      if(keyIndex >= (key.length())){
+      if (keyIndex >= (key.length())) {
         keyIndex = 0;
       }
 
-      if(alphabet.indexOf(currentChar) == -1){ // not a letter
+      if (alphabet.indexOf(currentChar) == -1) { // not a letter
         resultText += currentChar;
         continue;
       }
 
-      if(encrypt){
-        //original char index + key index; fe. "b"-> 1 + "s" -> 18 = 19 -> "t"
+      if (encrypt) {
+        // original char index + key index; fe. "b"-> 1 + "s" -> 18 = 19 -> "t"
         newIndex = alphabet.indexOf(currentChar) + alphabet.indexOf(key.charAt(keyIndex));
-        if(newIndex >= alphabet.length()){
+        if (newIndex >= alphabet.length()) {
           newIndex -= 26;
         }
-      }else{
+      } else {
         newIndex = alphabet.indexOf(currentChar) - alphabet.indexOf(key.charAt(keyIndex));
-        if(newIndex < 0){
+        if (newIndex < 0) {
           newIndex += 26;
         }
       }
@@ -88,15 +92,15 @@ public class vigenere{
 
     String answer = scanner.next();
 
-    if(answer.equals("c")){
+    if (answer.equals("c")) {
       return true;
-    }else if(answer.equals("e")){
+    } else if (answer.equals("e")) {
       text = "";
       return true;
-    }else if(answer.equals("u")){
+    } else if (answer.equals("u")) {
       text = resultText;
       return true;
-    }else if(answer.equals("s")){
+    } else if (answer.equals("s")) {
       text = "";
       encrypt = !encrypt;
       return true;
